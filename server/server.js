40954f71a9c77b6bd06efa96321b0773d32a1fe7
vanshikachauhan15 <vanshu.ch15@gmail.com>
2025-10-26 00:16:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ async function startServer() {
     }
   }
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 3000;
   server.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
   });
