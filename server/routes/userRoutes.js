57246d2e6ee04309a1b704f7cdeae24ef0dfd567
vanshikachauhan15 <vanshu.ch15@gmail.com>
2025-10-26 11:54:48 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -12,6 +12,12 @@ const userRouter = express.Router();
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
 userRouter.put("/update-profile", protectRoute, updateProfile);
-userRouter.get("/check", protectRoute, checkAuth);
+
+// 🧠 FIXED: In demo mode, /check route should work without token
+if (process.env.DEMO_MODE === "true") {
+  userRouter.get("/check", checkAuth);
+} else {
+  userRouter.get("/check", protectRoute, checkAuth);
+}
 
 export default userRouter;
